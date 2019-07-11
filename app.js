5a42fe5d192543b9bed62b8e8ef8b507e475e79d
Yuting Wen <blackalkane@gmail.com>
2019-07-10 21:28:43 -0700

    port bug fix2

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/messages', messagesRouter);
 
-app.listen(process.env.PORT || 9000, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 9000, () => console.log("Server running on port 9000"));
